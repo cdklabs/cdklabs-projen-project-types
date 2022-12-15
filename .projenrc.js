b24b86b3fbae60d11b55b1d9e6449a46c1b6ece1
Kaizen Conroy <zen.conroy@gmail.com>
2022-12-14 21:31:41 -0500

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,14 @@
+const { cdk } = require("projen");
+const project = new cdk.JsiiProject({
+  author: "AWS",
+  authorAddress: "aws-cdk-dev@amazon.com",
+  defaultReleaseBranch: "main",
+  name: "cdklabs-projen-project-types",
+  repositoryUrl: "https://github.com/cdklabs/cdklabs-projen-project-types.git",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
