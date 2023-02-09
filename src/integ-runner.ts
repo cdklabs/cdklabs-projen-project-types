1afea3d09de49ade5c9258e618594d4989b54131
Cory Hall <43035978+corymhall@users.noreply.github.com>
2023-02-09 15:11:47 -0500

    feat: add integ-runner and rosetta support (#17)
    This PR adds default support for integ-runner and rosetta for construct
    libraries. I've been making these changes individually for some repos
    (see https://github.com/cdklabs/cdk-stacksets), but we should be
    controlling it centrally.
    
    closes https://github.com/cdklabs/cdk-ops/issues/2438

---
+++ src/integ-runner.ts
@@ -0,0 +1,29 @@
+import { Component, DependencyType } from 'projen';
+import { TypeScriptProject } from 'projen/lib/typescript';
+
+/**
+ * This component adds support for using `integ-runner` and `integ-tests`
+ * in a construct library.
+ */
+export class IntegRunner extends Component {
+  constructor(project: TypeScriptProject) {
+    super(project);
+
+    project.deps.addDependency('@aws-cdk/integ-runner@^2.45.0', DependencyType.DEVENV);
+    project.deps.addDependency('@aws-cdk/integ-tests-alpha@^2.45.0-alpha.0', DependencyType.DEVENV);
+
+    const integSnapshotTask = project.addTask('integ', {
+      description: 'Run integration snapshot tests',
+      receiveArgs: true,
+      exec: 'yarn integ-runner --language typescript',
+    });
+
+    project.addTask('integ:update', {
+      description: 'Run and update integration snapshot tests',
+      exec: 'yarn integ-runner --language typescript --update-on-failed',
+      receiveArgs: true,
+    });
+
+    project.testTask.spawn(integSnapshotTask);
+  }
+}
